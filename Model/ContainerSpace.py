dd96a0425e28d3040c53161f073a47abb06c521c
SIvasu <kpsivaubramani5@gmail.com>
2025-07-03 23:33:22 +0530

    Added BiolerPlate going to split space paradigm in later commits

--- Model/ContainerSpace.py
+++ Model/ContainerSpace.py
@@ -3,6 +3,11 @@ from datetime import date
 from typing import List, Union, Optional
 
 class Cuboid:
+    """
+        Let Cuboid represent the basic unit of Space in a Container
+        A cuboid contains only one Cargo
+
+    """
     x : int
     y : int
     z : int
@@ -10,6 +15,7 @@ class Cuboid:
     height : int
     depth : int
     volume : int
+    cargo : Cargo | None
 
     def __init__(
             self, 
@@ -18,7 +24,8 @@ class Cuboid:
             z : int,
             height : int,
             depth : int,
-            widht : int
+            widht : int,
+            cargo: Cargo
     ):
         self.x = x
         self.y = y
@@ -27,41 +34,42 @@ class Cuboid:
         self.depth = depth
         self.widht = widht
         self.volume = self.height * self.widht * self.depth
+        self.cargo = cargo
 
-class UnitVolume:
-    """
-        Unit Volume represents the basic unit of Space in the container
-        Following the cartesian place, the units are unit cube with volume of 1 cm^3
+# class UnitVolume:
+#     """
+#         Unit Volume represents the basic unit of Space in the container
+#         Following the cartesian place, the units are unit cube with volume of 1 cm^3
 
-        For a container with dimensions [ width, height, depth ] => There are (width * height * depth) number of UnitVolume
+#         For a container with dimensions [ width, height, depth ] => There are (width * height * depth) number of UnitVolume
 
-        Each volume can be occupied by exactly one cargo
+#         Each volume can be occupied by exactly one cargo
 
-        #     ^
-        #     |
-        #     |
-        #     |
-        #     |
-        #     |
-        #     |
-        #     |___ ___ ___ ___ ___
-        #     | 1 | 2 | 3 | 4 | 5 | 
-        #     --------------------------
-        #     (0, 0)
-    """
-    cargo : Cargo
+#         #     ^
+#         #     |
+#         #     |
+#         #     |
+#         #     |
+#         #     |
+#         #     |
+#         #     |___ ___ ___ ___ ___
+#         #     | 1 | 2 | 3 | 4 | 5 | 
+#         #     --------------------------
+#         #     (0, 0)
+#     """
+#     cargo : Cargo
 
 
 
 class ContainerSpace:
-    SpaceMatrix: List[List[List["Cargo" | None]]]
+    # SpaceMatrix: List[List[List["Cargo" | None]]]
     itemsIncluded : List["Cargo"]
     width : int
     height : int
     depth : int
     spaceAvailable : int
     contiguousSpace : List[int]
-    largestCuboidsAvailabe : List[Cuboid]
+    largestCuboidAvailabe : List[Cuboid]
 
     def __init__(self, height, width, depth):
         self.height = height
@@ -78,10 +86,11 @@ class ContainerSpace:
             depth = depth,
             widht= width
         ))
-        self.SpaceMatrix = [ [ [ None for x in range(self.depth) ] for y in range(self.height) ] for z in range(self.width) ] 
+        # self.SpaceMatrix = [ [ [ None for x in range(self.depth) ] for y in range(self.height) ] for z in range(self.width) ] 
 
     def isSpaceFree(
             self, 
             cargo : Cargo,
     ):
         pass
+        
